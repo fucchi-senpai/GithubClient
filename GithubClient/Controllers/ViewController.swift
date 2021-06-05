a4cb55b991a597a2136e6f65a114f87c312d871f
Shota Fuchikami <0904kinazu@gmail.com>
2021-06-05 17:48:26 +0900

    first commit

---
+++ GithubClient/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubClient
+//
+//  Created by Shota Fuchikami on 2021/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# GithubClient
+
+## Environment
+
+- Xcode 12.5
+- Swift 5.4
+
+## Overview
+
+- Github Client App with UIKit
+
+## Package Manager
+
+- Mint 🌱
